d1f6c47a781657661351605bbc40d7847ac4ad1f
Adriangalicia <adriangalicia10@hotmail.com>
2024-05-23 06:42:26 -0500

    commit25

--- assets/js/carrusel.js
+++ assets/js/carrusel.js
@@ -1,13 +1,32 @@
-var swiper = new Swiper(".mySwiper", {
-
-    sliedesPerView: 1,
+var swiperLeft = new Swiper(".mySwiper-left", {
+    slidesPerView: 1,
     spaceBetween: 30,
-    grabCursor:true,
-    loop:true,
-    breakpoints : {
+    grabCursor: true,
+    loop: true,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
         991: {
-            slidesPerView:4
+            slidesPerView: 4
         }
     }
+});
 
+var swiperRight = new Swiper(".mySwiper-right", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    grabCursor: true,
+    loop: true,
+    autoplay: {
+        delay: 3000,
+        reverseDirection: true, // Hace que el carrusel se mueva en la dirección opuesta
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        991: {
+            slidesPerView: 4
+        }
+    }
 });
