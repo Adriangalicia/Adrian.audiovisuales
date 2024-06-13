7e2994d1799ca598f5cdace46110d9b331dcce68
Adriangalicia <adriangalicia10@hotmail.com>
2024-06-13 02:25:36 -0500

    commit33

--- assets/js/carrusel.js
+++ assets/js/carrusel.js
@@ -29,4 +29,33 @@ var swiperRight = new Swiper(".mySwiper-right", {
             slidesPerView: 4
         }
     }
+
 });
+
+var players = [];
+
+		function onYouTubeIframeAPIReady() {
+			var elements = document.getElementsByClassName('youtube-player');
+			for (var i = 0; i < elements.length; i++) {
+				var player = new YT.Player(elements[i], {
+					videoId: elements[i].dataset.id,
+					playerVars: {
+						'autoplay': 1,
+						'loop': 1,
+						'playlist': elements[i].dataset.id,
+						'mute': 1
+					},
+					events: {
+						'onReady': onPlayerReady
+					}
+				});
+				players.push(player);
+			}
+		}
+
+		function onPlayerReady(event) {
+			event.target.mute();
+		}
+
+
+
